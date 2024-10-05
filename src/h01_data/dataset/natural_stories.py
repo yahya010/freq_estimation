675e3a5cc380306af774ad8e40ec0e3fcb472e2b
Tiago Pimentel <tpimentelms@gmail.com>
2024-10-05 16:41:36 +0200

    Code to preprocess data and get surprisals

--- src/h01_data/dataset/natural_stories.py
+++ src/h01_data/dataset/natural_stories.py
@@ -1,12 +1,18 @@
+import bisect
+import numpy as np
 import pandas as pd
 
+from .base import BaseDataset
 from utils import utils
 
 
-class NaturalStoriesDataset():
+class NaturalStoriesDataset(BaseDataset):
+    unused_columns = ['WorkerId', 'WorkTimeInSeconds', 'correct', 'zone', 
+                      'meanItemRT', 'sdItemRT', 'gmeanItemRT', 'gsdItemRT',
+                      'word']
 
     @staticmethod
-    def get_text(input_path):
+    def get_stories_text(input_path):
         gpt3_probs = pd.read_csv("%s/all_stories_gpt3.csv" % (input_path))
 
         # To get same indexing as stories db
@@ -18,6 +24,43 @@ class NaturalStoriesDataset():
 
         # Concatenate words in a story, and stories together
         stories_df = gpt3_probs.groupby(by=["story"], sort=False).agg({"token_with_ws": utils.string_join}).reset_index()
-        stories = '\n'.join(stories_df['token_with_ws'].apply(lambda x: x.strip()).values)
+        # stories = '\n'.join(stories_df['token_with_ws'].apply(lambda x: x.strip()).values)
+        stories = list(zip(stories_df['story'], stories_df['token_with_ws']))
 
         return stories
+
+    @classmethod
+    def get_text(cls, input_path):
+        stories = cls.get_stories_text(input_path)
+        stories_text = [x[1].strip() for x in stories]
+
+        return '\n'.join(stories_text)
+    
+    @classmethod
+    def preprocess(cls, input_path):
+        # Get natural stories text
+        stories = cls.get_stories_text(input_path)
+        ns_stats = cls.get_corpus_stats(stories)
+
+        # Get natural stories RTs
+        df = pd.read_csv("%s/processed_RTs.tsv" % (input_path), sep='\t').drop_duplicates()
+        df.rename(columns={'RT': 'time', 'item': 'text_id'}, inplace=True)
+        df['word_id'] = df['zone'] - 1
+
+        # Exclude outliers
+        df = utils.find_outliers(df, transform=np.log)
+
+        # Create preprocessed dataframe
+        df = cls.create_analysis_dataframe(df, ns_stats)
+
+        # Check word matches ref_token (except for peeked vs peaked distinction)
+        assert ((df['word'] == df['ref_token']) | (df['word'] == 'peaked')).all()
+
+        # Deleted unused info from dataframe
+        cls.remove_unused_columns(df)
+        return df
+    
+    # @staticmethod
+    # def remove_unused_columns(df):
+    #     for col in unused_columns:
+    #         del df[col]
---
+++ src/h01_data/get_rt_with_surprisal_dataset.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import argparse
+import pandas as pd
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+# from dataset import NaturalStoriesDataset
+from h01_data.models import unigram
+from utils import utils
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    # Data
+    parser.add_argument('--surprisal-fname', type=str, required=True)
+    parser.add_argument('--rt-fname', type=str, required=True)
+    # Output
+    parser.add_argument('--output-fname', type=str, required=True)
+
+    return parser.parse_args()
+
+
+def merge_rt_and_surprisal(args):
+    merge_columns = ['text_id', 'word_id']
+
+    df_surprisals = pd.read_csv(args.surprisal_fname, sep='\t')
+    df_surprisals['text_id'] = df_surprisals['text_id'] + 1 # Fix text indexing
+
+    df_rt = pd.read_csv(args.rt_fname, sep='\t', index_col=0)
+    df_rt = df_rt[~df_rt.outlier]
+    del df_rt['outlier']
+    df_rt = df_rt.groupby(merge_columns + ['ref_token']).agg('mean').reset_index()
+
+    df = df_rt.set_index(merge_columns).join(
+        df_surprisals.set_index(merge_columns),
+        how='outer').reset_index()
+    
+    assert not df.surprisal.isna().any()
+    return df
+
+
+def get_frequencies(df):
+    df['freq'] = df['word'].apply(
+        lambda x: unigram.frequency(x))
+
+
+def get_spillover_vars(df):
+    for variable in ['word', 'surprisal', 'surprisal_buggy', 'freq', 'word_len']:
+        df['prev_' + variable] = df.groupby("text_id", sort=False)[variable].shift(periods=1, fill_value=None)
+        df['prev2_' + variable] = df.groupby("text_id", sort=False)[variable].shift(periods=2, fill_value=None)
+        df['prev3_' + variable] = df.groupby("text_id", sort=False)[variable].shift(periods=3, fill_value=None)
+
+def get_rt_with_surprisal_dataset(args):
+    df = merge_rt_and_surprisal(args)
+    get_frequencies(df)
+    get_spillover_vars(df)
+
+    return df
+
+
+def main():
+    args = get_args()
+    df = get_rt_with_surprisal_dataset(args)
+    utils.write_tsv(df, args.output_fname)
+
+
+if __name__ == '__main__':
+    main()
