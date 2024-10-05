a756afe2d6bc59ccf6bbd807f194911a7b6a11be
Tiago Pimentel <tpimentelms@gmail.com>
2024-10-06 00:50:04 +0200

    Fix gpt names in run all script

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for model in 'gpt-small' 'gpt-medium' 'gpt-large' 'gpt-xl' \
-             'pythia-70m' 'pythia-160m' 'pythia-410m' 'pythia-14b' 'pythia-28b' 'pythia-69b'
+for model in 'gpt2-small' 'gpt2-medium' 'gpt2-large' 'gpt2-xl' \
+             'pythia-70m' 'pythia-160m' 'pythia-410m' 'pythia-14b' 'pythia-28b'
+            #   'pythia-69b'
             #   'pythia-120b'
 do
     # for dataset in 'natural_stories' 'brown' 'provo_skip2zero' 'dundee_skip2zero'   
-    for dataset in 'natural_stories'
+    for dataset in 'natural_stories' 'provo' 'dundee'
     do
         make process_data MODEL=${model} DATASET=${dataset}
         make get_llh MODEL=${model} DATASET=${dataset}
