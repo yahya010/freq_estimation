544b62256f19e204086ef9d908b4384e7c213e94
Yahya Emara <49173233+yahya010@users.noreply.github.com>
2024-10-22 11:51:42 +0200

    added frequency integration

--- src/h01_data/get_rt_with_surprisal_dataset.py
+++ src/h01_data/get_rt_with_surprisal_dataset.py
@@ -2,7 +2,7 @@ import os
 import sys
 import argparse
 import pandas as pd
-
+from pathlib import Path
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 # from dataset import NaturalStoriesDataset
 from h01_data.models import unigram
@@ -43,17 +43,25 @@ def merge_rt_and_surprisal(args):
 def get_frequencies(df):
     df['freq'] = df['word'].apply(
         lambda x: unigram.frequency(x))
+    CURRENT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+    ROOT_DIR = CURRENT_DIR.parent.parent
+    # add if statement for each dataset
+    new_freq_path = Path(ROOT_DIR) / "corpora" / "rt" / "natstor_wiki_freqs.tsv"
+    df2 = pd.read_csv(new_freq_path, sep='\t')
+    df2 = df2.drop(columns=['Unnamed: 0'], errors='ignore')
+    df = pd.merge(df, df2, on='word', how='left')
+    return df
 
 
 def get_spillover_vars(df):
-    for variable in ['word', 'surprisal', 'surprisal_buggy', 'freq', 'word_len']:
+    for variable in ['word', 'surprisal', 'surprisal_buggy', 'freq', 'freq_full_wiki', 'freq_3_4_wiki', 'freq_half_wiki', 'freq_1_5_wiki', 'freq_1_10_wiki','word_len']:
         df['prev_' + variable] = df.groupby("text_id", sort=False)[variable].shift(periods=1, fill_value=None)
         df['prev2_' + variable] = df.groupby("text_id", sort=False)[variable].shift(periods=2, fill_value=None)
         df['prev3_' + variable] = df.groupby("text_id", sort=False)[variable].shift(periods=3, fill_value=None)
 
 def get_rt_with_surprisal_dataset(args):
     df = merge_rt_and_surprisal(args)
-    get_frequencies(df)
+    df = get_frequencies(df)
     get_spillover_vars(df)
 
     return df
