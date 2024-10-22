8557eae4fa5594df5b3171bdfaf6e0797e7bcc4a
Yahya Emara <49173233+yahya010@users.noreply.github.com>
2024-10-22 11:50:31 +0200

    added correct surprisal baselines.R

--- src/h02_rt_model/baselines.R
+++ src/h02_rt_model/baselines.R
@@ -1,7 +1,8 @@
 get_baselines <- function() {
   baselines <- c(
     hash('name'='empty', 'function'=''),
-    hash('name'='full_surprisal', 'function'='+surprisal_buggy+prev_surprisal_buggy+prev2_surprisal_buggy+prev3_surprisal_buggy')
+    # hash('name'='full_surprisal', 'function'='+surprisal_buggy+prev_surprisal_buggy+prev2_surprisal_buggy+prev3_surprisal_buggy')
+    hash('name'='full_surprisal', 'function'='+surprisal+prev_surprisal+prev2_surprisal+prev3_surprisal')
   )
 
   return(baselines)
